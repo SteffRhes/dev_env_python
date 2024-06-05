4b77d59f70e666ee9415347be34a0c67123c2812
SteffRhes <steffrhes@pm.me>
2024-06-05 23:10:04 +0200

    remove history since it's hardly used and more in the way in quick experiments

--- dev_env_python_run.sh
+++ dev_env_python_run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-if [ ! -e "./.dev_history" ]; then
-  mkdir .dev_history
-  touch .dev_history/bash_history
-  touch .dev_history/history.sqlite
-fi
-
-podman run \
-  -v ./.dev_history/bash_history:/root/.bash_history:rw \
-  -v ./.dev_history/history.sqlite:/root/.ipython/profile_default/history.sqlite:rw \
-  -v "$(pwd)":/mount \
-  -it --rm dev_env_python bash
+podman run -v "$(pwd)":/mount -it --rm dev_env_python bash
 
